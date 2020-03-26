79d34db4efee2dc1a1cc844e6a41764c7cb7af6c
Sean <11585008+Techatrekka@users.noreply.github.com>
2020-03-26 11:31:57 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,32 +1,120 @@
 public class Board {
 
-    Square[][] board = new Square[8][8];
+    Square[][] board;
 
     public Board() {
+        board = new Square[8][8];
         for (int i =0; i < 8; i++) {
-            for (int j = 0; i < 8; j++) {
+            for (int j = 0; j < 8; j++) {
                 board[i][j] = new Square();
             }
         }
-        int numOfOccupiedSquares = (int)(Math.random() * 4) + 3;
-        int startXPos = (int) Math.random() * 8,startYPos = (int) Math.random() * 8;
+        int obstacle = (int)(Math.random() * 3);
+        int startXPos = (int) (Math.random() * 8);
+        int startYPos = (int) (Math.random() * 8);
         board[startXPos][startYPos].changeSquareStatus();
-        int XPos, YPos;
-        for (int i = 0; i < numOfOccupiedSquares; i++) {
-            do {
-                XPos = (int) Math.random() * 8;
-                YPos = (int) Math.random() * 8;
-            } while (!board[XPos][YPos].getOccupied() && isBesideOccupiedSquare(XPos,YPos));
-            board[XPos][YPos].changeSquareStatus();
+        int orientation = (int) (Math.random() * 2);
+        System.out.print(orientation);
+        System.out.println();
+        System.out.print(obstacle);
+        System.out.println();
+        switch (obstacle) {
+            case 0:
+                //I shape
+                if (orientation == 0) {
+                    for (int i = 1; Math.abs(i) < 3; Math.abs(i++)) {
+                        if (startYPos+i > 7){
+                            i *= -1;
+                        }
+                        board[startXPos][startYPos+i].changeSquareStatus();
+                        if (i < 0) {
+                            i*=-1;
+                        }
+                    }
+                } else {
+                    for (int i = 1; Math.abs(i) < 3; Math.abs(i++)) {
+                        if (startXPos+i > 7) {
+                            i*= -1;
+                        }
+                        board[startXPos+i][startYPos].changeSquareStatus();
+                        if (i < 0) {
+                            i*=-1;
+                        }
+                    }
+                }
+                break;
+            case 1:
+                // L shape
+                if (orientation == 0) {
+                    for (int i = 1; Math.abs(i) < 3; Math.abs(i++)) {
+                        if (startYPos+i > 7){
+                            i *= -1;
+                        }
+                        board[startXPos][startYPos+i].changeSquareStatus();
+                        if (i < 0) {
+                            i*=-1;
+                        }
+                    }
+                    if (startXPos + 1 > 7) {
+                        board[startXPos-1][startYPos].changeSquareStatus();
+                    } else {
+                        board[startXPos+1][startYPos].changeSquareStatus();
+                    }
+                } else {
+                    for (int i = 1; Math.abs(i) < 3; Math.abs(i++)) {
+                        if (startXPos+i > 7){
+                            i *= -1;
+                        }
+                        board[startXPos+i][startYPos].changeSquareStatus();
+                        if (i < 0) {
+                            i*=-1;
+                        }
+                    }
+                    if (startYPos + 1 > 7) {
+                        board[startXPos][startYPos-1].changeSquareStatus();
+                    }else {
+                        board[startXPos][startYPos+1].changeSquareStatus();
+                    }
+                }
+                break;
+            case 2:
+                //T shape
+
+                break;
         }
+        /*int XPos, YPos;
+        for (int i = 1; i < numOfOccupiedSquares; i++) {
+            do {
+                XPos = (int) (Math.random() * 8);
+                YPos = (int) (Math.random() * 8);
+            } while (!board[XPos][YPos].getOccupied() && !isBesideOccupiedSquare(XPos,YPos));
+            board[XPos][YPos].changeSquareStatus();
+        }*/
     }
 
     private boolean isBesideOccupiedSquare(int xPos, int yPos) {
-        if (board[Math.abs(xPos-1)][yPos].getOccupied() || board[xPos+1][yPos].getOccupied()) {
+        if (board[Math.abs(xPos-1)][yPos].getOccupied()) {
             return true;
-        } else if (board[xPos][Math.abs(yPos-1)].getOccupied() || board[xPos][yPos+1].getOccupied()) {
+        } else if (board[xPos][Math.abs(yPos-1)].getOccupied()) {
             return true;
+        } else if (xPos !=7) {
+            if (board[xPos+1][yPos].getOccupied()) {
+                return true;
+            }
+        } else if (yPos != 7) {
+            if (board[xPos][yPos+1].getOccupied()) {
+                return true;
+            }
         }
         return false;
+    }
+
+    void displayBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j].getOccupied());
+            }
+            System.out.println();
+        }
     }
 }
